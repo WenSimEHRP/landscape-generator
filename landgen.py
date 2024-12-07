eb6ec5de7579da93c63a91d5dcb133bad15589c8
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-12-06 21:10:46 -0800

    Update: allow generating diagonal textures

--- landgen.py
+++ landgen.py
@@ -3,23 +3,22 @@ import masks
 import os
 import argparse
 
-class TransformImage:
 
-    def __init__(self, path, resolution=1, strength=1):
+class TransformImage:
+    def __init__(self, path, resolution=1, strength=1, diagonal=False):
         self.path = path
         self._img = None  # original image
         self._imgs = None  # transformed images
         self.resolution = resolution
         self.strength = strength
+        self.diagonal = diagonal
 
     @property
     def img(self):
         if self._img is None:
             with Image.open(self.path) as img:
                 # resize the image to square shaped, use nearest neighbour to keep the pixel sharp
-                self._img = img.resize(
-                    (2 * 64 * self.resolution,) * 2
-                )
+                self._img = img.resize((2 * 64 * self.resolution,) * 2)
         return self._img
 
     @property
@@ -36,9 +35,27 @@ class TransformImage:
 
     def apply_transform(self):
         d = {}
-        for i in range(4):
+        for i in range(8 if self.diagonal else 4):
             deg = i * 90
-            img = self.img.copy().rotate(deg)
+            if i >= 4:
+                # create a larger version of the image first, paste the original image 9 times, rotate it, and crop it
+                img = self.img.copy().resize((self.img.width * 3, self.img.height * 3))
+                for x in range(3):
+                    for y in range(3):
+                        img.paste(self.img, (self.img.width * x, self.img.height * y))
+                # rotate the image
+                img = img.rotate(deg + 45)
+                # crop the image and zoom
+                img = img.crop(
+                    (
+                        self.img.width,
+                        self.img.height,
+                        self.img.width * 2,
+                        self.img.height * 2,
+                    )
+                )
+            else:
+                img = self.img.copy().rotate(deg)
             for ind, val in enumerate(masks.TRANSFORM_MATRIX.items()):
                 name, matrix = val
                 d[name] = {**d.get(name, {}), i: None}
@@ -124,26 +141,32 @@ class TransformImage:
         return a
 
 
-def compose_images(img: TransformImage, mask):
+def compose_images(img: TransformImage, mask, diagonal_slopes=False):
     d = {}
-    for i in range(4):
+    t = {}
+    for i in range(len(next(iter(img.imgs.values())))):
         compose = {}
         for mi, mv in mask.items():
-            # mi = the mask type
-            # mv = the mask path
             m = Image.open("masks/compound/" + mv)
-            a = img.imgs[mi][i].copy()
-            a = a.convert("RGBA")
+            c = i
+            if diagonal_slopes and "cliff" in mi:
+                if "lower" in mv or "upper" in mv:
+                    print("yes")
+                    c += 3
+                c = (c + 4) % 8
+            a = img.imgs[mi][c].copy().convert("RGBA")
             a.putalpha(m.convert("L"))
-            # crop
             bbox = a.getbbox()
             if bbox:
                 a = a.crop(bbox)
             for n in ("right", "left", "upper", "lower"):
                 if n in mv:
                     compose[n] = a
+        t[i] = compose
+
+    for i, compose in t.items():
+
         if "right" in compose and "left" in compose:
-            # merge the right and left images side by side
             new_width = compose["right"].width + compose["left"].width
             new_height = max(compose["right"].height, compose["left"].height)
             new_img = Image.new("RGBA", (new_width, new_height))
@@ -151,7 +174,6 @@ def compose_images(img: TransformImage, mask):
             new_img.paste(compose["right"], (compose["left"].width, 0))
             d[i] = new_img
         elif "upper" in compose and "lower" in compose:
-            # merge the upper and lower images top and bottom
             new_width = max(compose["upper"].width, compose["lower"].width)
             new_height = compose["upper"].height + compose["lower"].height - 1
             new_img = Image.new("RGBA", (new_width, new_height))
@@ -159,44 +181,79 @@ def compose_images(img: TransformImage, mask):
             new_img.paste(compose["lower"], (0, compose["upper"].height - 1))
             d[i] = new_img
         else:
-            print(
-                compose.keys(),
-            )
             raise ValueError("Invalid mask")
     return d
 
 
-def main(input_path, output_path, strength = 0.25):
+def main(args):
     try:
-        img = TransformImage(input_path, 1, strength)
+        img = TransformImage(
+            args.input_path, args.resolution, args.strength, args.diagonal
+        )
     except FileNotFoundError:
         print("Invalid path")
         return
     except Exception as e:
         print(e)
         return
-    for i, v in masks.mask_list.items():
-        compose_images(img, v)
-    output_imgs = {i: compose_images(img, v) for i, v in masks.mask_list.items()}
     output_imgs = {
-        **output_imgs,
+        **{
+            i: compose_images(img, v, args.diagonal_on_slopes)
+            for i, v in masks.mask_list.items()
+        },
         **{masks.order_translation[i]: v for i, v in img.imgs.items()},
     }
+    if args.diagonal_on_slopes:
+        output_imgs[12][0] = output_imgs[12][1]
+        output_imgs[3][0] = output_imgs[3][1]
+        output_imgs[15][0] = output_imgs[15][5]
+        output_imgs[16][0] = output_imgs[16][7]
+        output_imgs[17][0] = output_imgs[17][6]
+        output_imgs[18][0] = output_imgs[18][4]
     for ind, val in output_imgs.items():
         for i, v in val.items():
             v = v.convert("RGBA")
-            v.save(f"{output_path}/{i}/" + f"{ind}" + ".png")
+            v.save(f"{args.output_dir}/{i}/" + f"{ind}" + ".png")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some images')
-    parser.add_argument('-i', '--input-path', type=str, required=True, help='Path to the input file')
-    parser.add_argument('-o', '--output-dir', type=str, required=True, help='Directory to save the output')
-    parser.add_argument('-s', '--strength', type=float, default=0.25, help='Strength value (suggested: 0.25)')
-    parser.add_argument('-d', '--diagonal', type=bool, default=False, help='Output diagonal images')
-    parser.add_argument('-n', '--diagonal-on-slopes', type=bool, default=False, help='Output diagonal images on slopes')
-    parser.add_argument('-r', '--resolution', type=int, default=1, help='Resolution of the output image')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some images")
+    parser.add_argument(
+        "-i", "--input-path", type=str, required=True, help="Path to the input file"
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        type=str,
+        required=True,
+        help="Directory to save the output",
+    )
+    parser.add_argument(
+        "-s",
+        "--strength",
+        type=float,
+        default=0.25,
+        help="Strength value (suggested: 0.25)",
+    )
+    parser.add_argument(
+        "-d",
+        "--diagonal",
+        action="store_true",
+        default=False,
+        help="Output diagonal textures",
+    )
+    parser.add_argument(
+        "-n",
+        "--diagonal-on-slopes",
+        action="store_true",
+        default=False,
+        help="Output diagonal textures on slopes",
+    )
+    parser.add_argument(
+        "-r", "--resolution", type=int, default=1, help="Resolution of the output image"
+    )
     args = parser.parse_args()
 
-    for i in range(4):
+    for i in range(8 if args.diagonal else 4):
         os.makedirs(f"{args.output_dir}/{i}/", exist_ok=True)
-    main(args.input_path, args.output_dir, args.strength)
+    main(args)
--- readme.md
+++ readme.md
@@ -5,14 +5,17 @@ A simple command-line landscape generator for use with OpenTTD.
 Usage:
 
 ```sh
-python3 <input_path> <output_path> <shadow_strength>
+python3 landgen.py [-h] -i <INPUT_PATH> -o <OUTPUT_DIR> [-s <STRENGTH>] [-d] [-n] [-r <RESOLUTION>]
 ```
 
-- `input_path`: the path to the image
-- `output_path`: where to put the output
-- `shadow_strength`: controls shadow strength
+- `-i INPUT_PATH, --input-path`: Path to the input file
+- `-o OUTPUT_DIR, --output-dir`: Directory to save the output
+- `-s STRENGTH, --strength`: Strength value (suggested: 0.25)
+- `-d, --diagonal`: Output diagonal textures
+- `-n, --diagonal-on-slopes`: Output diagonal textures on slopes
+- `-r RESOLUTION, --resolution RESOLUTION`: Resolution of the output image (WIP)
 
-This program produces 4 set of landscape sprite (4*19=76 sprites in total), with each set facing a different direction.
+This program produces 4 or 8 sets of landscape sprite (76 or 152 sprites in total), with each set facing a different direction.
 The output is in **32bpp 1x**, so make sure to either remap the colours or encode 32bpp flags into your GRF.
 
 Licensed under the MIT license.
